d5984be10ab23cf6e51896477525c3113df2c6cc
wucangeo <wucangeo@gmail.com>
2015-08-08 14:40:07 +0800

    修改渲染地图的模式为join

--- SnowCover/frmSetStaMapDate.cs
+++ SnowCover/frmSetStaMapDate.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.IO;
+
 using ESRI.ArcGIS.Controls;
 using ESRI.ArcGIS.Carto;
 using ESRI.ArcGIS.Geodatabase;
@@ -34,21 +36,22 @@ namespace SnowCover
         private void btn_submit_Click(object sender, EventArgs e)
         {
             this.Hide();
-            frmProgressBar frmPB = new frmProgressBar();
-            frmPB.Show();
+            //frmProgressBar frmPB = new frmProgressBar();
+            //frmPB.Show();
             date = this.dateNavigator1.DateTime;
             string dateStr = date.ToString("yyyy-MM-dd");
-            Dictionary<string, SnowData> snowStaDataKeyMap = getDataFromDatabase(dateStr);
-            if (snowStaDataKeyMap == null||snowStaDataKeyMap.Count==0)
+            DataTable snowDataTable = getDataFromDatabase(dateStr);
+            if (snowDataTable == null || snowDataTable.Rows.Count == 0)
             {
-                frmPB.Close();
+                //frmPB.Close();
                 MessageBox.Show("未获取到[" + dateStr + "]的数据，请重试。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 this.Show();
                 return;
             }
-            ILayer layer = mapControl.Map.get_Layer(0);
-            EditRendererLayerAttr(layer, snowStaDataKeyMap);
-            frmPB.Close();
+            ExportDataToExcel(snowDataTable, @"D:\Develop\data\SnowCover\maps\SnowCover_t.xls");
+            //ILayer layer = mapControl.Map.get_Layer(0);
+            //EditRendererLayerAttr(layer, snowStaDataKeyMap);
+            //frmPB.Close();
             mapControl.ActiveView.Refresh();
             this.Close();
         }
@@ -60,72 +63,86 @@ namespace SnowCover
         }
 
         //从数据库获取数据
-        private Dictionary<string, SnowData> getDataFromDatabase(string  dateStr)
+        private DataTable getDataFromDatabase(string  dateStr)
         {
-            Dictionary<string, SnowData> snowDataKeyMap = new Dictionary<string, SnowData>();
+            DataTable table = new DataTable();
             try
             {
-                string sqlStr = "SELECT	sn.Code as snCode,sn.COUNT AS snCount,sn.SUM as snSum "
+                string sqlStr = "SELECT	sn.Code as scode,sn.COUNT AS scount,sn.SUM as ssum,sn.SUM/sn.COUNT*100 AS spercent,sn.Date AS sdate "
                     + "FROM snowcover_statisticbyboundary AS sn "
                     + "WHERE sn.Date LIKE '%" + dateStr + "%' "
                     + "ORDER BY sn.Code";
-                DataTable dt = SystemBase.MySQL.Select(sqlStr, mysqlConn);
-                if (dt.Rows.Count == 0)
+                table = SystemBase.MySQL.Select(sqlStr, mysqlConn);
+                if (table.Rows.Count == 0)
                 {
-                    return snowDataKeyMap;
+                    return table;
                 }
-                foreach (DataRow row in dt.Rows)
-                {
-                    SnowData sd = new SnowData();
-                    sd.code = (string)row[0];
-                    sd.count = (int)row[1];
-                    sd.sum = (int)row[2];
-                    sd.date = dateStr;
+                table.TableName = "stacounty";
+                //foreach (DataRow row in snowDataTable.Rows)
+                //{
+                //    SnowData sd = new SnowData();
+                //    sd.code = (string)row[0];
+                //    sd.count = (int)row[1];
+                //    sd.sum = (int)row[2];
+                //    sd.date = dateStr;
 
-                    snowDataKeyMap.Add(sd.code, sd);
-                }
+                //    snowDataKeyMap.Add(sd.code, sd);
+                //}                
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            return snowDataKeyMap;
+            return table;
         }
 
         //修改图层属性的值
         private void EditRendererLayerAttr(ILayer layer, Dictionary<string, SnowData> snowDataKeyMap)
         {
+            //IFeatureLayer pFTLayer = layer as IFeatureLayer;
+            ////IFeatureClass pFTClass = pFTLayer.Search                ITable pTable = pFTClass as ITable;
+            //IQueryFilter queryFilter = new QueryFilterClass();
+            //queryFilter.WhereClause = "Code <> ''";
+            //IFeatureCursor pFTCursor = pFTLayer.Search(queryFilter, false);
+            ////pCursor = pTable.Search(null, false);
+            ////IRow row = pFTCursor.NextRow();
+            //IFeature feature = pFTCursor.NextFeature();
+
+            IFeatureLayer pFTLayer = layer as IFeatureLayer;
+            ITable pTbale = pFTLayer as ITable;
+            IQueryFilter queryFilter = new QueryFilterClass();
+            queryFilter.WhereClause = "Code <> ''";
+            ICursor pFTCursor = pTbale.Search(queryFilter, false);
+            IRow row = pFTCursor.NextRow();            
+
             try
             {
-                IFeatureLayer pFTLayer = layer as IFeatureLayer;
-                //IFeatureClass pFTClass = pFTLayer.Search                ITable pTable = pFTClass as ITable;
-                IQueryFilter queryFilter = new QueryFilterClass();
-                queryFilter.WhereClause = "Code <> ''";
-                IFeatureCursor pFTCursor = pFTLayer.Search(queryFilter, false);
-                //pCursor = pTable.Search(null, false);
-                //IRow row = pFTCursor.NextRow();
-                IFeature feature = pFTCursor.NextFeature();
-                int codeIndex = feature.Fields.FindField("Code");
-                int countIndex = feature.Fields.FindField("count");
-                int sumIndex = feature.Fields.FindField("sum");
-                int dateIndex = feature.Fields.FindField("date");
-                int percentIndex = feature.Fields.FindField("percent");
-
-                while (feature != null)
+                int codeIndex = row.Fields.FindField("Code");
+                int countIndex = row.Fields.FindField("count");
+                int sumIndex = row.Fields.FindField("sum");
+                int dateIndex = row.Fields.FindField("date");
+                int percentIndex = row.Fields.FindField("percent");
+                int countFT = 0;
+                while (row != null)
                 {
-                    string code = (string)feature.get_Value(codeIndex);
-                    SnowData sn = snowDataKeyMap[code];
+                    string code = (string)row.get_Value(codeIndex);
+                    SnowData sn = null;
+                    snowDataKeyMap.TryGetValue(code, out sn);
+                    if (sn == null)
+                    {
+                        continue;
+                    }
                     if (countIndex > 0)
                     {
-                        feature.set_Value(countIndex, sn.count);
+                        row.set_Value(countIndex, sn.count);
                     }
                     if (sumIndex > 0)
                     {
-                        feature.set_Value(sumIndex, sn.sum);
+                        row.set_Value(sumIndex, sn.sum);
                     }
                     if (dateIndex > 0)
                     {
-                        feature.set_Value(dateIndex, sn.date);
+                        row.set_Value(dateIndex, sn.date);
                     }
                     double percent = 0.0;
                     if (sn.sum != 0 && sn.count != 0)
@@ -134,18 +151,66 @@ namespace SnowCover
                     }
                     if (percentIndex > 0)
                     {
-                        feature.set_Value(percentIndex, percent);
+                        row.set_Value(percentIndex, percent);
                     }
-                    feature.Store();
-                    feature = pFTCursor.NextFeature();
+                    row.Store();
+                    row = pFTCursor.NextRow();
+                    countFT++;
                 }
+                System.Diagnostics.Debug.Write("Count:"+countFT);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            finally
+            {
+                System.GC.Collect();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(pFTCursor);
+            }
         }
 
+        private void ExportDataToExcel(DataTable dt,string excelPath)
+        {
+            try
+            {
+                StreamWriter wr = new StreamWriter(excelPath);
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    wr.Write(dt.Columns[i].ToString().ToUpper() + "\t");
+                }
+
+                wr.WriteLine();
+
+                //write rows to excel file
+                for (int i = 0; i < (dt.Rows.Count); i++)
+                {
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        if (dt.Rows[i][j] != null)
+                        {
+                            wr.Write(Convert.ToString(dt.Rows[i][j]) + "\t");
+                        }
+                        else
+                        {
+                            wr.Write("\t");
+                        }
+                    }
+                    //go to next line
+                    wr.WriteLine();
+                }
+                //close file
+                wr.Close();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
     }
     public class SnowData
